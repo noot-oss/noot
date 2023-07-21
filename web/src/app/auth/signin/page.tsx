1987d5e44fb6895f72f632dd51c7382470cc2460
JoltCode <joe@joecc.dev>
2023-07-21 12:25:56 +0100

    fix(web/auth): signin providers

--- web/src/app/auth/signin/page.tsx
+++ web/src/app/auth/signin/page.tsx
@@ -1,26 +1,15 @@
 import { type SignInServerPageParams } from "next-auth/core/pages/signin";
-import { getProviders } from "next-auth/react";
 import { SignInInner } from "~/components/auth/signin/SignInInner";
-import { Card } from "@nextui-org/react";
 
-const SignInPage = async (props: {
+const SignInPage = (props: {
   params: SignInServerPageParams;
   searchParams: {
     callbackUrl: string;
   };
 }) => {
-  const providers = await getProviders();
-
-  if (!providers) {
-    return <div>Providers not found</div>;
-  }
-
   return (
     <div className="flex grow flex-col items-center justify-center px-4 sm:px-0">
-      <SignInInner
-        providers={providers}
-        callbackUrl={props.searchParams.callbackUrl}
-      />
+      <SignInInner callbackUrl={props.searchParams.callbackUrl} />
     </div>
   );
 };
--- web/src/components/auth/signin/SignInInner.tsx
+++ web/src/components/auth/signin/SignInInner.tsx
@@ -1,27 +1,40 @@
 "use client";
 
-import { type Provider } from "next-auth/providers";
-import { type getProviders, signIn, useSession } from "next-auth/react";
+import { getProviders, signIn, useSession } from "next-auth/react";
 import { Button, Card } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export interface SIgnInInnerProps {
-  providers: Awaited<ReturnType<typeof getProviders>>;
   callbackUrl: string;
 }
+
+type ProvidersReturned = Awaited<ReturnType<typeof getProviders>>;
 
 export const SignInInner = (props: SIgnInInnerProps) => {
   const session = useSession();
   const router = useRouter();
 
+  const [providers, setProviders] = useState<ProvidersReturned>(null);
+
+  useEffect(() => {
+    getProviders()
+      .then((providers) => setProviders(providers))
+      .catch((err) => console.error(err));
+  }, []);
+
+  if (!providers) {
+    return <div>Providers not found</div>;
+  }
+
   if (session.data) router.push(props.callbackUrl);
 
-  if (!props.providers) return <div>Providers not found</div>;
+  if (!providers) return <div>Providers not found</div>;
 
   return (
     <Card className="mx-4 flex w-full flex-col gap-8 px-8 py-16 text-center sm:mx-0 sm:max-w-xs md:max-w-md md:px-32 lg:max-w-2xl">
       <h1 className="text-4xl">Sign In</h1>
-      {Object.values(props.providers).map((provider, index) => {
+      {Object.values(providers).map((provider, index) => {
         return (
           <Button
             key={index}
