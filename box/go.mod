26e07357d901ba41c41049d4e49b37e1b4f89a71
Ashley Caramel <BISCUITISNOTACOOKIE@protonmail.com>
2023-07-21 02:58:18 +0100

    commented loads of code :thumbsup:

--- box/go.mod
+++ box/go.mod
@@ -2,6 +2,12 @@ module github.com/fwuffyboi/noot
 
 go 1.20
 
-require github.com/sirupsen/logrus v1.9.3
+require (
+	github.com/aldernero/scd4x v0.0.0-20220130180236-4b75adf24948
+	github.com/sirupsen/logrus v1.9.3
+)
 
-require golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+require (
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	periph.io/x/conn/v3 v3.6.10 // indirect
+)
--- box/main.go
+++ box/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	log "github.com/sirupsen/logrus" // for logging
-	"io"
-	"net/http"
 	"os"
 )
 
@@ -15,13 +13,18 @@ func main() {
 	log.SetLevel(log.InfoLevel)
 	log.Info("Logging initiated!")
 
-	// This chunk of code goes to the url below and requests the message to print when the nootBOX starts up
+
+	// THIS BIT IS COMMENTED OUT FOR A POTENTIAL FUTURE FEATURE.
+
+	/*// This chunk of code goes to the url below and requests the message to print when the nootBOX starts up
 	// The message could be an ascii art, just some text, possibly an important update or something silly, like a
 	// greeting that says "Happy Christmas" around december.
+
 	resp, err := http.Get("https://msgsrvnoot.fluffywolff.repl.co/box/startupmessage/ml")
+
 	// if there is an error connecting to get the startup message, then say there's an error and use the one built in
 	if err != nil {
-		log.Error("Failed to get startup message. REASON: Connection failed. ERROR: %s", err)
+		// log.Error("Failed to get startup message. REASON: Connection failed. ERROR: %s", err)
 		log.Info("Falling back to default startup message.")
 
 		fmt.Printf(`
@@ -56,8 +59,10 @@ func main() {
 		// if code was not 200 (OK), then use the default welcome banner thingy
 		log.Error("Failed to get startup message. REASON: Status code was not '200'.")
 		log.Info("Falling back to default startup message.")
+	*/
 
-		fmt.Printf(`
+	// Print this cool thing.
+	fmt.Printf(`
  ███╗   ██╗ ██████╗  ██████╗ ████████╗██████╗  ██████╗ ██╗  ██╗
  ████╗  ██║██╔═══██╗██╔═══██╗╚══██╔══╝██╔══██╗██╔═══██╗╚██╗██╔╝
  ██╔██╗ ██║██║   ██║██║   ██║   ██║   ██████╔╝██║   ██║ ╚███╔╝ 
@@ -65,6 +70,16 @@ func main() {
  ██║ ╚████║╚██████╔╝╚██████╔╝   ██║   ██████╔╝╚██████╔╝██╔╝ ██╗
  ╚═╝  ╚═══╝ ╚═════╝  ╚═════╝    ╚═╝   ╚═════╝  ╚═════╝ ╚═╝  ╚═╝
 `)
+
+
+	// Check if we have a BoxID stored.
+	// No box id? start enroll procedure with a small gin webserver that says your temporary code
+	// Yes, there is a box id? Continue
+	// Try to login
+	// login failed? start enroll procedure with gin webserver
+
+
+
 	}
 
-}
+// }
