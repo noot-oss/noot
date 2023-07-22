4ff681aa10341d1db044d953109e4de439068109
JoltCode <joe@joecc.dev>
2023-07-22 17:49:14 +0100

    feat(web/create): box code multi input

--- web/src/components/create/CreateBoxCode.tsx
+++ web/src/components/create/CreateBoxCode.tsx
@@ -1,13 +1,84 @@
 "use client";
 
-import { Input } from "@nextui-org/react";
-import { useEffect, useState } from "react";
-import { Button } from "@nextui-org/react";
+import { Button, Input } from "@nextui-org/react";
+import { createRef, useEffect, useState } from "react";
 import { z } from "zod";
 
+const CODEINPUT_LENGTH = 8;
+
 const codeSchema = z.object({
-  code: z.string().min(0).max(6),
+  code: z.string().min(0).max(9),
 });
+
+const CodeInput = () => {
+  const [code, setCode] = useState<string[]>(Array.from({ length: 8 }));
+  const [refs, setRefs] = useState<React.RefObject<HTMLInputElement>[]>([]);
+
+  useEffect(() => {
+    Array.from({ length: CODEINPUT_LENGTH }).forEach(() => {
+      setRefs((refs) => [...refs, createRef()]);
+    });
+  }, []);
+
+  return (
+    <div className={"grid grid-cols-8 gap-8"}>
+      {Array.from({ length: CODEINPUT_LENGTH }).map((_, i) => {
+        return (
+          <Input
+            key={i}
+            type="number"
+            min="0"
+            max="10"
+            pattern={"[0-9]*"}
+            size={"lg"}
+            classNames={{
+              input: "text-center text-4xl font-bold",
+              inputWrapper: "w-16 h-32",
+            }}
+            variant={"bordered"}
+            ref={refs[i]}
+            placeholder={"0"}
+            value={code[i] ?? "0"}
+            onKeyDown={(e) => {
+              e.preventDefault();
+              if (e.key.match(/[0-9]/)) {
+                setCode((prev) => {
+                  return prev.map((v, index) => {
+                    if (index === i) {
+                      return e.key;
+                    } else {
+                      return v;
+                    }
+                  });
+                });
+
+                if (i < CODEINPUT_LENGTH - 1) {
+                  refs[i + 1]?.current?.focus();
+                }
+              }
+
+              if (e.key === "Backspace") {
+                setCode((prev) => {
+                  return prev.map((v, index) => {
+                    if (index === i) {
+                      return "";
+                    } else {
+                      return v;
+                    }
+                  });
+                });
+
+                if (i > 0) {
+                  refs[i - 1]?.current?.focus();
+                }
+              }
+            }}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
 export const CreateBoxCode = () => {
   const [code, setCode] = useState("");
@@ -22,23 +93,9 @@ export const CreateBoxCode = () => {
   return (
     <div className="mt-8 flex flex-col gap-8 lg:mt-32">
       <h2 className="text-2xl">Enter Box Code</h2>
-      <Input
-        type="number"
-        inputMode="numeric"
-        pattern="[0-9]*"
-        variant="bordered"
-        aria-label="Box code"
-        className="md:max-w-xl lg:max-w-6xl"
-        max={6}
-        classNames={{
-          input: "text-2xl lg:text-4xl text-center",
-          inputWrapper: "h-16 lg:h-32",
-        }}
-        validationState={isValid ? "valid" : "invalid"}
-        errorMessage={!isValid && "That doesn't look like a valid code..."}
-        value={code}
-        onChange={(e) => setCode(e.target.value)}
-      />
+
+      <CodeInput />
+
       <Button variant="solid" size="lg" color="primary">
         Continue
       </Button>
