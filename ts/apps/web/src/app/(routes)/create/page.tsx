9d779086b1554cffe5f7fc240f48cf7fce3076f4
JoltCode <joe@joecc.dev>
2023-07-31 18:42:11 +0100

    feat(web/create): initial page layout, get code from db

--- ts/apps/web/src/app/(routes)/create/page.tsx
+++ ts/apps/web/src/app/(routes)/create/page.tsx
@@ -1,30 +1,66 @@
 import { CreateBoxLoadedCode } from "~/components/create/CreateBoxLoadedCode";
-import { useSession } from "next-auth/react";
 import { getServerAuthSession } from "~/server/auth";
 import crypto from "node:crypto";
 import { prisma } from "~/server/db";
 
+const StepSection = (props: { children: React.ReactNode }) => (
+  <section className="flex flex-col gap-4">{props.children}</section>
+);
+
+const StepTitle = (props: { children: React.ReactNode }) => (
+  <h2 className="text-2xl font-bold">{props.children}</h2>
+);
+
+const CreatePageInner = (props: { code: string }) => (
+  <div className="flex h-full flex-col items-center">
+    <h1 className="text-center text-4xl font-bold md:text-6xl">Create A Box</h1>
+    <h2 className="my-4 text-center text-xl">
+      It{"'"}s so easy to set up a new NootBox! Just follow our 3 step guide and
+      <br />
+      you{"'"}ll be on your way to monitoring your environment!
+    </h2>
+    <div className="mt-16 flex w-full flex-col gap-16 px-64 lg:grid lg:grid-cols-3 lg:grid-rows-1">
+      <StepSection>
+        <StepTitle>1. Plug in your NootBox</StepTitle>
+        {/*TODO: Animation of plugging in NootBox*/}
+        <div className="h-96 w-full bg-white/10"></div>
+      </StepSection>
+      <StepSection>
+        <StepTitle>2. Visit myip:1234</StepTitle>
+        {/*TODO: Animation of plugging in NootBox*/}
+        <div className="h-96 w-full bg-white/10"></div>
+      </StepSection>
+      <StepSection>
+        <StepTitle>3. Enter the following code:</StepTitle>
+        <CreateBoxLoadedCode verificationCode={props.code} />
+      </StepSection>
+    </div>
+  </div>
+);
+
 const CreatePage = async () => {
   const session = await getServerAuthSession();
 
+  const userBoxInit = await prisma.boxInit.findFirst({
+    where: {
+      creatorId: session.user.id,
+    },
+  });
+
+  if (userBoxInit.verificationCode) {
+    return <CreatePageInner code={userBoxInit.verificationCode} />;
+  }
+
   const EightDigitCode = crypto.randomInt(10000000, 99999999);
 
-  // const boxInit = await prisma.boxInit.create({
-  //   data: {
-  //     verificationCode: EightDigitCode.toString(),
-  //     creatorId: session.user.id,
-  //   },
-  // });
+  const boxInit = await prisma.boxInit.create({
+    data: {
+      verificationCode: EightDigitCode.toString(),
+      creatorId: session.user.id,
+    },
+  });
 
-  return (
-    <div className="flex h-full flex-col items-center gap-16">
-      <h1 className="text-center text-4xl font-bold md:text-6xl">
-        Create A Box
-      </h1>
-      {/*<CreateBoxLoadedCode verificationCode={boxInit.verificationCode} />*/}
-      Coming soon
-    </div>
-  );
+  return <CreatePageInner code={boxInit.verificationCode} />;
 };
 
 export default CreatePage;
--- ts/apps/web/src/components/create/CreateBoxLoadedCode.tsx
+++ ts/apps/web/src/components/create/CreateBoxLoadedCode.tsx
@@ -1,14 +1,17 @@
 "use client";
 
-import { Code, Spinner } from "@nextui-org/react";
+import { Code } from "@nextui-org/react";
+import { Snippet } from "@nextui-org/react";
 
 export const CreateBoxLoadedCode = (props: { verificationCode: string }) => {
+  const codeToDisplay =
+    props.verificationCode.substring(0, 4) +
+    "-" +
+    props.verificationCode.substring(4);
+
   return (
-    <>
-      <Code>
-        {props.verificationCode.substring(0, 4)}-
-        {props.verificationCode.substring(4)}
-      </Code>
-    </>
+    <Snippet className={"w-1/2 lg:w-full"} hideSymbol>
+      {codeToDisplay}
+    </Snippet>
   );
 };
