5c9553491d64e84f646657c721bc554c706cc540
JoltCode <joe@joecc.dev>
2023-08-05 12:58:17 +0100

    fix: remove github sha

--- ts/apps/web/src/env.mjs
+++ ts/apps/web/src/env.mjs
@@ -20,7 +20,6 @@ export const env = createEnv({
     //   Upstash
     UPSTASH_REDIS_REST_URL: z.string().min(1),
     UPSTASH_REDIS_REST_TOKEN: z.string().min(1),
-    GITHUB_SHA: process.env.NODE_ENV === "production" ? z.string().min(1) : z.string().max(0).optional(),
   },
 
   /**
@@ -45,7 +44,6 @@ export const env = createEnv({
     DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
     UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
     UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
-    GITHUB_SHA: process.env.GITHUB_SHA,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
