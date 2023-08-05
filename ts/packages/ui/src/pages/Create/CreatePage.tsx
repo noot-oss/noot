b7c3fb174b7cf5f2ea1942110cc989421ee9d012
JoltCode <joe@joecc.dev>
2023-08-05 18:29:06 +0100

    feat(ts/ui): CreatePage continue button, fix styling

--- ts/packages/ui/src/pages/Create/CreatePage.tsx
+++ ts/packages/ui/src/pages/Create/CreatePage.tsx
@@ -1,4 +1,6 @@
 import { CreateBoxLoadedCode } from "./CreateBoxLoadedCode";
+import { Button } from "~ui/components/Button";
+import { H1 } from "~ui/components/Typography";
 
 const StepSection = (props: { children: React.ReactNode }) => (
   <section className="flex flex-col gap-4">{props.children}</section>
@@ -10,26 +12,29 @@ const StepTitle = (props: { children: React.ReactNode }) => (
 
 export const CreatePageInner = (props: { code: string }) => (
   <div className="flex h-full flex-col items-center">
-    <h1 className="text-center text-4xl font-bold md:text-6xl">Create A Box</h1>
+    <H1>Create A Box</H1>
     <h2 className="my-4 text-center text-xl">
       It{"'"}s so easy to set up a new NootBox! Just follow our 3 step guide and
       <br />
       you{"'"}ll be on your way to monitoring your environment!
     </h2>
-    <div className="mt-16 flex w-full flex-col gap-16 px-64 lg:grid lg:grid-cols-3 lg:grid-rows-1">
+    <div className="mt-16 flex w-full flex-col gap-16 md:grid md:grid-cols-3 md:grid-rows-1">
       <StepSection>
         <StepTitle>1. Plug in your NootBox</StepTitle>
         {/*TODO: Animation of plugging in NootBox*/}
-        <div className="h-96 w-full bg-white/10"></div>
+        <div className="h-96 w-full rounded-md bg-white/10"></div>
       </StepSection>
       <StepSection>
         <StepTitle>2. Visit myip:1234</StepTitle>
         {/*TODO: Animation of plugging in NootBox*/}
-        <div className="h-96 w-full bg-white/10"></div>
+        <div className="mt-auto h-96 w-full rounded-md bg-white/10"></div>
       </StepSection>
       <StepSection>
         <StepTitle>3. Enter the following code:</StepTitle>
         <CreateBoxLoadedCode verificationCode={props.code} />
+        <Button className={"mt-2"} size={"lg"}>
+          Continue
+        </Button>
       </StepSection>
     </div>
   </div>
