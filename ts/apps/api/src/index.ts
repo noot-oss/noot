848cf0a49399ddaae5ecd3e87d6c90ff817390da
JoltCode <joe@joecc.dev>
2023-08-09 16:03:45 +0100

    feat(ts/api): provide env vars to redis ratelimiter

--- ts/apps/api/src/index.ts
+++ ts/apps/api/src/index.ts
@@ -2,23 +2,31 @@ import { Hono } from "hono";
 import { Redis } from "@upstash/redis/cloudflare";
 import { Ratelimit } from "@upstash/ratelimit";
 
-const app = new Hono();
+type EnvVars = {
+  UPSTASH_REDIS_REST_URL: string;
+  UPSTASH_REDIS_REST_TOKEN: string;
+};
 
-const redis = Redis.fromEnv();
-
-const ratelimit = new Ratelimit({
-  redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(10, "10 s"),
-  analytics: true,
-  /**
-   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
-   * instance with other applications and want to avoid key collisions. The default prefix is
-   * "@upstash/ratelimit"
-   */
-  prefix: "@upstash/ratelimit/api",
-});
+const app = new Hono<{
+  Bindings: EnvVars;
+}>();
 
 app.use("*", async (context, next) => {
+  const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv({
+      UPSTASH_REDIS_REST_URL: context.env.UPSTASH_REDIS_REST_URL,
+      UPSTASH_REDIS_REST_TOKEN: context.env.UPSTASH_REDIS_REST_TOKEN,
+    }),
+    limiter: Ratelimit.slidingWindow(10, "10 s"),
+    analytics: true,
+    /**
+     * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+     * instance with other applications and want to avoid key collisions. The default prefix is
+     * "@upstash/ratelimit"
+     */
+    prefix: "@upstash/ratelimit/api",
+  });
+
   // TODO: Rate limit by identifier/ip
   const success = await ratelimit.limit("all");
 
