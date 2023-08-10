76b7c276532f69905a19435eff378f97df1908d4
JoltCode <joe@joecc.dev>
2023-08-10 12:49:40 +0100

    fix(ts/db): edge -> non edge

--- ts/packages/db/index.ts
+++ ts/packages/db/index.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client/edge";
+import { PrismaClient } from "@prisma/client";
 import { Redis } from "@upstash/redis";
 
 const globalForPrisma = globalThis as unknown as {
