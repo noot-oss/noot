432a1bc0a928c4e340f98c2be44f0df49047897d
JoltCode <joe@joecc.dev>
2023-09-04 21:51:16 +0100

    feat(web): analytics

--- ts/apps/web/src/app/layout.tsx
+++ ts/apps/web/src/app/layout.tsx
@@ -3,10 +3,20 @@
 import "~web/styles/globals.css";
 import { api } from "~web/utils/api";
 import { Providers } from "~web/app/providers";
+import Script from "next/script";
+import { env } from "~web/env.mjs";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" suppressHydrationWarning>
+      {env.NODE_ENV === "production" && (
+        <Script
+          async
+          src="https://analytics.umami.is/script.js"
+          data-website-id={env.NEXT_PUBLIC_UMAMI_CODE}
+        />
+      )}
+
       <body className="flex h-full flex-col">
         <Providers>{children}</Providers>
       </body>
--- ts/apps/web/src/env.mjs
+++ ts/apps/web/src/env.mjs
@@ -29,6 +29,7 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
+    NEXT_PUBLIC_UMAMI_CODE: z.string().min(1),
   },
 
   /**
