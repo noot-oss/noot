ffa6fbe3c400ba72f0bdc34ddc200e6b7d5cdac1
Ashley Caramel <BISCUITISNOTACOOKIE@protonmail.com>
2023-09-09 10:59:35 +0100

    Update SignInInner.tsx

--- ts/apps/web/src/components/auth/signin/SignInInner.tsx
+++ ts/apps/web/src/components/auth/signin/SignInInner.tsx
@@ -24,7 +24,7 @@ export const SignInInner = () => {
   }, []);
 
   if (!providers) {
-    return <div>Providers not found</div>;
+    return <div>Loading login providers</div>;
   }
 
   if (session.data) router.push("/");
