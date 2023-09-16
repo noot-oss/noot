25de729e29468a205a8eb23b20614e5775a6750c
Ashley Caramel <BISCUITISNOTACOOKIE@protonmail.com>
2023-09-16 19:06:14 +0000

    Update Footer.tsx

--- ts/packages/ui/src/components/Footer.tsx
+++ ts/packages/ui/src/components/Footer.tsx
@@ -15,7 +15,7 @@ export const FooterInner = (
   return (
     <footer className="flex min-h-[4rem] w-full flex-col items-center justify-center gap-2 border-t px-4 sm:flex-row sm:justify-between lg:gap-0 lg:px-16">
       <span>
-        NWVersion: {PRODUCTION ? "PROD_" : "DEVELOPMENT"}{PRODUCTION && (
+        NootWeb_Version: {PRODUCTION ? "PROD_" : "DEVELOPMENT"}{PRODUCTION && (
           <span>{props.NEXT_PUBLIC_GITHUB_SHA.substring(0, 7)}</span>
       )}
       </span>
