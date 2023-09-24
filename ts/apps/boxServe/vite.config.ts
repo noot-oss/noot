f1fd3f4c040a96fe47c23aa84d9fc765b3caac26
JoltCode <joe@joecc.dev>
2023-09-24 19:37:28 +0100

    fix(boxServe): paths, deps

--- ts/apps/boxServe/vite.config.ts
+++ ts/apps/boxServe/vite.config.ts
@@ -4,5 +4,10 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths()],
+  plugins: [
+    react(),
+    viteTsconfigPaths({
+      root: "../../",
+    }),
+  ],
 });
